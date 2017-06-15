a1fca703c12a61ac47ec278a49ef14825e689079
Tim Thomas <tithomas@cisco.com>
2017-06-15 11:04:54 -0400

    Add copyright, comment about which import to use

--- library/ydkMACsecModule.py
+++ library/ydkMACsecModule.py
@@ -7,8 +7,24 @@
 # MACsec keychain configuration on a target IOS-XR device
 #
 # Note the module tries to be idempotent...
-
+#
 # TODO: More detailed DOCUMENTATION string
+#
+# ------------------------------------------------------------------
+# Copyright 2017 Cisco Systems
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------
 
 DOCUMENTATION = """
 ---
@@ -79,7 +95,12 @@ try:
     from ydk.types import Empty
     from ydk.providers import NetconfServiceProvider
     from ydk.services import CRUDService
+
+    # I'm importing the MACsec keychain model from a smaller, generated API. Flip the
+    # import if using the Cisco IOS-XR models that come with ydk-py
+
     from ydk.models.ciscolive_ansible_macsec import Cisco_IOS_XR_lib_keychain_macsec_cfg
+    # from ydk.models.cisco_ios_xr import Cisco_IOS_XR_lib_keychain_macsec_cfg
     YDK_AVAILABLE = True
 except ImportError:
     YDK_AVAILABLE = False
